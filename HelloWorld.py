c8d3eb8c20d4f51ea4e6832e19a0bf8e1b9c6818
Rahul Chaudhary <30838739+rahulchaudharydeveloper@users.noreply.github.com>
2018-04-10 08:38:31 +0530

    Create HelloWorld.py
    Different ways of printing Hello World in python.

---
+++ HelloWorld.py
@@ -0,0 +1,18 @@
+#print hello world in python 
+print "hello world"
+print ("hello world")
+print 'hello world'
+
+
+# '''--------do anything---------'''
+print ('''hello 
+   world
+     i am interesting
+
+ ''')
+
+print ('''hello 
+world
+i am interesting
+
+''')
