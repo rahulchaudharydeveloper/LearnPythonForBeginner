d6efa0a2c3691949ee734c98fbdb35418cf1cac3
yogi6591 <33276799+yogi6591@users.noreply.github.com>
2018-10-31 21:18:47 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -16,3 +16,11 @@ world
 i am interesting
 
 ''')
+
+
+
+a="hello world"
+print(a) #here i am storing the string i.e. hello world in the variable "a" and then i am printing it so it will print hello world here
+
+list=["hello world","this is me"]
+print(list[0])   #this will print hello world as at 0th index of list we have "hello world"  
